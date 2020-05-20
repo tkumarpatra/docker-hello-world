4dad598bcef2a6befe9ed9c59700de371b02cb91
Tanmay <tanmay.kumar.patra@rabobank.nl>
2020-05-20 22:57:53 +0530

    first commit

---
+++ src/main/java/com/tanmay/docker/dockerhelloworld/DockerHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.tanmay.docker.dockerhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanmay/docker/dockerhelloworld/DockerHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmay.docker.dockerhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
